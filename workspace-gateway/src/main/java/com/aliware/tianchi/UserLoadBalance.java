7fe92f52e3293aaecae18a4812171a679aec982d
gongdavi <gongdavi@163.com>
2019-07-12 11:28:47 +0800

    1

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -31,29 +31,42 @@ public class UserLoadBalance implements LoadBalance {
     public static BigDecimal largeThread = new BigDecimal(0);
     public static BigDecimal mediumThread = new BigDecimal(0);
 
+    private static final int serverNum = 3;
+    //线程Map，key=服务标识，value=服务对应的线程数
     public static Map<String, Integer> threadCountMap = new HashMap<String, Integer>();
-    public static String[] servers = new String[3];
-    public static Integer[] weight = new Integer[3];
-    public static Integer weightSum = 0;
+
+    //权重相关
+    public static String[] servers = new String[3];//服务器
+    public static Integer[] weight = new Integer[3];//服务器的权重
+    public long weightDuration = 1000;//多久计算一次权重
     public static long time0 = System.currentTimeMillis();
-    public long duration = 1000;//多久计算一次权重
+    public static Integer weightCount;//权重的和
+
+
+    //响应时间，key=服务标识，value=每个响应的时间、响应的时长
+    public static Map<String, Map<Long, Integer>> rspTimeMap = new HashMap<String, Map<Long, Integer>>();
 
 //    private static final
 
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-        if (threadCountMap.size() < 3 || threadCountMap.values().iterator().next() < 50) {
+        if (threadCountMap.size() < serverNum || threadCountMap.values().iterator().next() < 50) {
             return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
         } else {
             long curTime = System.currentTimeMillis();
-            if (curTime - time0 > duration) {
+            if (curTime - time0 > weightDuration) {
                 //重新计算权重
+                weightCount = 0;
+                for (int i = 0; i < serverNum; i++) {
+                    weight[i] = threadCountMap.get(servers[i]);
+                    weightCount += weight[i];
+                }
             }
-            int randomValue = ThreadLocalRandom.current().nextInt(invokers.size());
+            int randomValue = ThreadLocalRandom.current().nextInt(weightCount);
             int curValue = 0;
             String serverFlag = "";
-            for(int i=0; i<servers.length; i++) {
-                curValue = curValue+weight[i];
+            for(int i=0; i<serverNum; i++) {
+                curValue += weight[i];
                 if (randomValue <= curValue) {
                     serverFlag = servers[i];
                     break;
